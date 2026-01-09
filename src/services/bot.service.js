c63f98bc2074571197ad595ee45cbad3dc80f221
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2026-01-08 21:11:49 -0300

    add request to service for test

--- src/services/bot.service.js
+++ src/services/bot.service.js
@@ -1,6 +1,12 @@
 class BotService {
-  processMessage(message) {
+  async processMessage(message) {
     // message.content.toLowerCase() === "!hola"
+    const response = await fetch(process.env.SERVICE_URL, {
+      method: "GET",
+    });
+
+    const data = await response.json();
+    console.log("Response from external service:", data);
     return "Message processed successfully!!!";
   }
 }
