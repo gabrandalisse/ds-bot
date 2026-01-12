d4af9ddec64583c9dd46dd863dbc316b07b88826
Gabriel Andres Brandalisse <gabibrandalisse@gmail.com>
2026-01-11 23:42:46 -0300

    simplify code

--- src/services/bot.service.js
+++ src/services/bot.service.js
@@ -1,41 +1,41 @@
-const {
-  CarryWatchChannelStrategy,
-  WebScrappingChannelStrategy,
-  AuctionWatchChannelStrategy,
-} = require("../infrastructure/strategies/channel.strategy");
-
 class BotService {
-  strategy = null;
-
   async processMessage(message) {
     if (!message.content.includes("!execute"))
       throw new Error("message without valid command");
 
+    let url;
     switch (message.channelId) {
       case process.env.CARRY_WATCH_CHANNEL_ID:
-        this.strategy = new CarryWatchChannelStrategy();
+        url = process.env.CARRY_WATCH_FLOW_URL;
         break;
 
       case process.env.WEB_SRAPPING_CHANNEL_ID:
-        this.strategy = new WebScrappingChannelStrategy();
+        url = process.env.WEB_SRAPPING_FLOW_URL;
         break;
 
       case process.env.AUCTION_WATCH_CHANNEL_ID:
-        this.strategy = new AuctionWatchChannelStrategy();
+        url = process.env.AUCTION_WATCH_FLOW_URL;
         break;
 
       default:
         throw new Error("invalid channel id");
     }
 
-    if (!this.strategy) throw new Error("strategy not found");
-
-    const response = await this.strategy.execute();
+    if (!url) throw new Error("url not found");
+    const response = await this.makeHTTPRequest(url);
 
     console.log("response from external service:", response);
 
     if (response.received) response = "message processed successfully.";
     else throw new Error("unexpected response received");
+  }
+
+  async makeHTTPRequest(url) {
+    const response = await fetch(url, {
+      method: "GET",
+    });
+
+    return await response.json();
   }
 }
 
